1846d96ba557889e3d33fdc7f73ba7fc9dbe41fd
Mane <manebabakhanyan11.ait@gmail.com>
2023-12-08 19:27:19 +0400

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <div className='border-[1px]'>
+          <img src="https://www.list.am/img/p.png" alt="" className='h-[40px]'/>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
