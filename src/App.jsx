5081f2b603493736856bb5aa5d351baf9576080d
Mane <manebabakhanyan11.ait@gmail.com>
2023-12-10 17:54:52 +0400

    Tneri vacharq

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import '../src/index.css'
 import Header from './Header/header';
 import Main from './Main/main';
 import BiznesEjer from './Biznes Ejer/biznesEjer';
+import TneriVacharq from './Tneri vacharq/tneriVacharq';
 function App() {
   return (
     <div>
       <Header />
       <Main />
       <BiznesEjer />
+      <TneriVacharq />
     </div>
   );
 }
---
+++ src/Tneri vacharq/tneriVacharq.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+export default function TneriVacharq() {
+    return (
+        <div>
+            <div>
+                <h1 className='font-bold text-[8px] pl-[70px] hover:text-blue2 inline'>Տների վաճառք</h1>
+            </div>
+            <div className='flex pt-[10px] pl-[70px] gap-[3px]'>
+                <div>
+                    <img src="https://s.list.am/h/932/76114932.webp" className='rounded object-cover w-[60px] h-[60px]' alt="" />
+                    <p className='text-[5.5px]'>250,000$</p>
+                    <p className='text-[5.5px] w-[60px] hover:text-blue2'>Մեկ հարկանի քարե տուն</p>
+                    <p className='text-[4.5px] text-grey'>6 սեն., 447 ք.մ.</p>
+                </div>
+                <div>
+                    <img src="https://s.list.am/h/965/76584965.webp" className='rounded object-cover w-[60px] h-[60px]' alt="" />
+                    <p className='text-[5.5px]'>115,000$</p>
+                    <p className='flex flex-wrap text-[5.5px] w-[60px] hover:text-blue2'>Մեկ հարկանի քարե տուն Փարաքարում</p>
+                    <p className='text-[4.5px] text-grey'>2 սեն., 49 ք.մ.</p>
+                </div>
+                <div>
+                    <img src="https://s.list.am/h/808/57124808.webp" className='rounded object-cover w-[60px] h-[60px]' alt="" />
+                    <p className='text-[5.5px]'>340,000$</p>
+                    <p className='flex flex-wrap text-[5.5px] w-[60px] hover:text-blue2'>Երեք հարկանի քարե տուն, Հաղթանակի 1...</p>
+                    <p className='text-[4.5px] text-grey'>3 սեն., 555 ք.մ.</p>
+                </div>
+                <div>
+                    <img src="https://s.list.am/h/496/74969496.webp" className='rounded object-cover w-[60px] h-[60px]' alt="" />
+                    <p className='text-[5.5px]'>74,000,000 ֏</p>
+                    <p className='flex flex-wrap text-[5.5px] w-[60px] hover:text-blue2'>Երկու հարկանի թաունհաուզ...</p>
+                    <p className='text-[4.5px] text-grey'>4 սեն., 153 ք.մ.</p>
+                </div>
+                <div>
+                    <img src="https://s.list.am/h/441/64523441.webp" className='rounded object-cover w-[60px] h-[60px]' alt="" />
+                    <p className='text-[5.5px]'>$170,000</p>
+                    <p className='flex flex-wrap text-[5.5px] w-[60px] hover:text-blue2'>Երկու հարկանի ամառանոց Դարանի...</p>
+                    <p className='text-[4.5px] text-grey'>5 սեն., 1030 ք.մ.</p>
+                </div>
+                <div>
+                    <img src="https://s.list.am/h/641/71104641.webp" className='rounded object-cover w-[60px] h-[60px]' alt="" />
+                    <p className='text-[5.5px]'>$590,000</p>
+                    <p className='flex flex-wrap text-[5.5px] w-[60px] hover:text-blue2'>Երեք հարկանի քարե տուն Արագածի...</p>
+                    <p className='text-[4.5px] text-grey'>5 սեն., 450 ք.մ.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
