7654970bfcdc28d3734e137bdd8944088df54626
Mane <manebabakhanyan11.ait@gmail.com>
2023-12-10 16:09:50 +0400

    Biznes ejer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css';
+import '../src/index.css'
 import Header from './Header/header';
 import Main from './Main/main';
+import BiznesEjer from './Biznes Ejer/biznesEjer';
 function App() {
   return (
     <div>
       <Header />
       <Main />
+      <BiznesEjer />
     </div>
   );
 }
---
+++ src/Biznes Ejer/biznesEjer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../index.css'
+export default function BiznesEjer() {
+    return (
+        <div>
+            <div>
+                <h1 className='font-bold text-[8px]  pl-[70px] hover:text-blue2 inline'>Բիզնես էջեր</h1>
+            </div>
+            <div className='pl-[70px] pt-[5px]'>
+                <div className='w-[40px] flex'>
+                    <img src="https://upa.list.am/13012.jpg" alt="" />
+                    <img src="https://upa.list.am/14336.jpg" className='ml-[8px]' alt="" />
+                    <img src="https://upa.list.am/5199.jpg" className='ml-[8px] rounded' alt="" />
+                    <img src="https://upa.list.am/10496.jpg" className='ml-[8px] rounded' alt="" />
+                    <img src="https://upa.list.am/14079.jpg" className='ml-[8px] rounded' alt="" />
+                    <img src="https://upa.list.am/14658.jpg" className='ml-[8px] rounded' alt="" />
+                    <img src="https://upa.list.am/10326.jpg" className='ml-[8px] rounded' alt="" />
+                    <img src="https://upa.list.am/7727.jpg" className='ml-[8px] rounded' alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
